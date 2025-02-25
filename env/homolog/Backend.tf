4b36e06a79a219b693b6b8bb8faa15dd9102a54a
Fabio Siqueira <fabio.siqueira@picpay.com>
2025-02-25 20:25:19 -0300

    add: tf files

---
+++ env/homolog/Backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "fast-food-terraform-state"
+    key    = "eks-cluster/terraform.tfstate"
+    region = "us-east-1"
+    profile = "fast_food"
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,46 @@
+variable "awsRegion" {
+    default = "us-east-1"
+}
+
+variable "profile" {
+    default = "fast_food"
+}
+
+
+variable "cidr_block" {
+    type = string
+    default = "10.10.0.0/16"
+}
+
+variable "projectName" {
+    default = "fast-food"
+}
+
+variable "instanceType" {
+    default = "t3.medium"
+}
+
+variable "minSize" {
+    default = 1
+}
+
+variable "maxSize" {
+    default = 2
+}
+
+variable "desiredSize" {
+    default = 1
+}
+
+variable "eksVersion" {
+    default = "1.32"
+}
+
+variable "tags" {
+    type = map(string)
+    default = {
+        terraform  = "true"
+        kubernetes = "fast-food-eks-cluster"
+    }
+    description = "Tags to apply to all resources"
+}
