c0d872a69c530654c004b969ce9c181da25fbc4a
Fabio Siqueira <fabio22.siqueira@gmail.com>
2025-05-11 16:06:02 -0300

    feat: ingress and secret provider (#7)

--- infra/v1/iam.tf
+++ infra/v1/iam.tf
@@ -86,3 +86,46 @@ resource "aws_eks_access_entry" "manager" {
   kubernetes_groups = ["my-admin"]
 }
 
+
+resource "aws_iam_policy" "secrets_manager_read_only" {
+  name = "SecretsManagerReadOnly"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret"
+      ]
+      "Resource": "*"
+    }]
+  })
+}
+
+resource "aws_iam_role" "pod_assume_role" {
+  name = "EKSPodAssumeRole"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "pods.eks.amazonaws.com"
+        },
+        "Action": [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.pod_assume_role.name
+  policy_arn = aws_iam_policy.secrets_manager_read_only.arn
+}
---
+++ infra/v1/secrets_provider.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "csi-secrets-store" {
+  name       = "csi-secrets-store"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+
+  set {
+    name  = "syncSecret.enabled"
+    value = "true"
+  }
+}
+
+resource "helm_release" "secrets-provider-aws" {
+  name       = "secrets-provider-aws"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
+}
